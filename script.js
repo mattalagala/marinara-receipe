504d11d9c3e577ee4c3adf1843ae5b7e9c188387
Alicia Alagala <aliciaalagala@alicias-mbp.lan>
2020-01-25 15:29:30 -0600

    Completd Marinara Receipe Part 2 - Using Parameters

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 // hi - I am a comment 
 
-var x = 4
+var x = 4;
 // 2) Write out a function definition called marinaraSauce. Be sure to be mindful of your syntax!
 
-function marinaraSauce () {
+function marinaraSauce (cookName) {
 
-    console.log("Heat a medium-large saucepan over medium heat.")
-    console.log("Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes.")
-    console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.")
-    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes")
-    console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed")
+    console.log("Hey, " + cookName + "! This is my marinara sauce receipe!");
+    console.log("Heat a medium-large saucepan over medium heat.");
+    console.log("Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes.");
+    console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.");
+    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
+    console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed");
 
 }
 
-marinaraSauce()
+marinaraSauce("Adam")
+
+//Part 1 - Writing a Receipe
 
 //3) Inside this function, write a series of console.log(); statements that will print the following marinara sauce recipe:
 
@@ -30,3 +33,14 @@ marinaraSauce()
 //4) After your function definition, write the line of code that will invoke your function
 
 //5) Run your script with node script.js. You should see your recipe printed to the screen!
+
+//Part 2- Using parameters
+
+//1) In your function definition, change marinaraSauce to take one parameter called cookName
+
+//2) Use cookName to print "Hey, cookName! This is my marinara sauce recipe!" before the recipe
+
+//3) Change your function invocation to take "Adam" as a cookName
+
+//4) When you run your script, you should see  "Hey, Adam! This is my marinara sauce recipe!", followed by the recipe
+
