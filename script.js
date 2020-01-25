174e6de615ce7e99e0bc879133b8241b4ef8a686
Alicia Alagala <aliciaalagala@Alicias-MacBook-Pro.local>
2020-01-25 13:33:22 -0600

    Completed Marinara Receipe Part 1 Exercise

---
+++ script.js
@@ -0,0 +1,32 @@
+// hi - I am a comment 
+
+var x = 4
+// 2) Write out a function definition called marinaraSauce. Be sure to be mindful of your syntax!
+
+function marinaraSauce () {
+
+    console.log("Heat a medium-large saucepan over medium heat.")
+    console.log("Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes.")
+    console.log("Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.")
+    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes")
+    console.log("Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed")
+
+}
+
+marinaraSauce()
+
+//3) Inside this function, write a series of console.log(); statements that will print the following marinara sauce recipe:
+
+//  - Heat a medium-large saucepan over medium heat.
+
+//    - Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes
+
+//    - Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.
+
+//    - Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes
+
+//    - Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.
+
+//4) After your function definition, write the line of code that will invoke your function
+
+//5) Run your script with node script.js. You should see your recipe printed to the screen!
